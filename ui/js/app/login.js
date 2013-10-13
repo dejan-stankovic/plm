e54ae40e456ab293cde66db15b7f2a3e07b74e39
Christian Heckendorf <heckendorfc@gmail.com>
2013-10-13 11:23:32 -0400

    Adding login service integration.

--- ui/js/app/login.js
+++ ui/js/app/login.js
@@ -6,6 +6,64 @@
 **************************************************************************/
 
 /************************************************************
+Function name: loginHandler
+Author: Christian Heckendorf, Rachit
+Created date: 10/13/2013
+Purpose: Processes the login form
+************************************************************/
+function loginHandler() {
+	var user, pass;
+
+	$("#errormsg").html("");
+
+	user = $("input#user").val();
+	pass = $("input#pass").val();
+
+	$.ajax({
+		type: 'POST',
+		url: '/plm/rest/login',
+		contentType: 'application/json; charset=UTF-8',
+		accepts: {
+			text: 'application/json'
+		},
+		dataType: 'json',
+		data: JSON.stringify({
+			name: user,
+			password: pass
+		}),
+		success: function(data) {
+			/*the following code checkes whether the entered userid and password are matching*/
+			if(data.code==0) {
+				setToken(data.message);
+				window.location = "dashboard.html"; /*opens the target page while Id & password matches*/
+			}
+			else if(data.code==1) {
+				$("#errormsg").html("Invalid Username or Password"); /*displays error message*/
+			}
+			else { /* Usually internal error or other */
+				$("#errormsg").html(data.message);
+			}
+
+		},
+		error: function(data){
+			alert("error");
+		}
+	});
+}
+
+/************************************************************
+Function name: cancelHandler
+Author: Christian Heckendorf
+Created date: 10/13/2013
+Purpose: Cancels the login form
+************************************************************/
+function cancelHandler() {
+	$("input#user").val("");
+	$("input#pass").val("");
+	$("#errormsg").html("");
+}
+
+/************************************************************
 		Function name: onready
 		Author: Rachit
 		Created date: 09/30/2013
@@ -28,6 +86,14 @@ $(document).ready(function () {
     }
     window.data("kendoWindow").center().open();
     setTimeout(function () { window.data("kendoWindow").close(); document.getElementById("body").style.display = ""; }, 3000);
+
+	$("a#loginbtn").click(function(){
+		loginHandler();
+	})
+
+	$("a#cancelbtn").click(function(){
+		cancelHandler();
+	})
 });
 
 
