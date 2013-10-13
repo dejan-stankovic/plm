4fb36627b3593d0e8c829107e8af8b9b4475afbc
yuvarajboston <yuvarajg@bu.edu>
2013-10-13 18:35:39 -0400

    registration edited

--- ui/js/app/registration.js
+++ ui/js/app/registration.js
@@ -1,13 +1,13 @@
 ﻿/*************************************************************************
 		File name: registration.js
-		Author: Yuvraj
+		Author: Yuvaraj
 		Created date: 09/30/2013
 		Purpose: client functionality associated with registration page
 **************************************************************************/
 
 /************************************************************
 		Function name: onready
-		Author: Yuvraj
+		Author: Yuvaraj
 		Created date: 09/30/2013
 		Purpose: ready function invoked when page is rendered
 **************************************************************/
