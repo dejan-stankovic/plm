c0989a39f6600926ed162a68ef6977f9ab764750
manavbarua <manavb.17@gmail.com>
2013-11-03 10:53:18 -0500

    user story ui development

---
+++ ui/js/data/serviceResponse.js
@@ -0,0 +1,9 @@
+﻿{
+    "userStories":[{"id":1,"name":"NewRandom 0.028741610702127218","description":"Some description","points":1,"owner":{"id":2,"name":"anotheruser"},"status":{"id":2,"name":"Pending"}},{"id":2,"name":"Random 0.5523067144677043","description":"Some description","points":1,"owner":{"id":1,"name":"auser"},"status":{"id":1,"name":"Initial"}}],
+    "statuses":[{"id":1,"name":"Initial"},{"id":2,"name":"Pending"},{"id":3,"name":"InProgress"},{"id":4,"name":"Complete"}],
+    "comments" : [{ "UserStoryId" : 1, "Id": 1, "Comments": "shall we use third party tool for login", "Creator": "Manav" },{ "UserStoryId": 2, "Id": 3, "Comments": "comments1", "Creator": "Vipul" }],
+    "releases" : ["Release 13.01.01", "Release 13.01.02", "Release 13.01.03"],
+    "message" : "success token message",
+    "code" : 0
+}
+
