a15b63bf52d26a33b68ca7e72e4f253ddb1b5fa4
Christian Heckendorf <heckendorfc@gmail.com>
2013-11-07 15:35:52 -0500

    Updating hours worked plot in presentation.

--- documentation/Iteration2/hours_metrics.R
+++ documentation/Iteration2/hours_metrics.R
@@ -3,7 +3,15 @@
 library(ggplot2)
 library(reshape2)
 
-df <- data.frame(week=1:8, Manav=c(7,8,14,11,13,10,17,0), Vipul=c(6,10,11,13,14,10,10,0),Yuvaraj=c(5,13,17,15,27,6,6,0),Rachit=c(5,8,15,13,14,9,14,0),Alan=c(3,10,9,14,13,3,0,0),Christian=c(12,21,28,25,31,10,14,26),Tandhy=c(7,8,7,12,20,10,8,0))
+df <- data.frame(week=1:8,
+		Manav=c(7,8,14,11,13,10,17,10), # 8 guess
+		Vipul=c(6,10,11,13,14,10,10,8),
+		Yuvaraj=c(5,13,17,15,27,6,6,6),
+		Rachit=c(5,8,15,13,14,9,14,8),
+		Alan=c(3,10,9,14,13,3,6,7),
+		Christian=c(12,21,28,25,31,10,14,26),
+		Tandhy=c(7,8,7,12,20,10,8,9))
+
 df <- melt(df, id.vars='week')
 
 g <- ggplot(df, aes(week, value, group=variable)) +
