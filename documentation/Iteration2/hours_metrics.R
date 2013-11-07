c91ae0217463d75b21d75af71af1b732f97210dc
Christian Heckendorf <heckendorfc@gmail.com>
2013-11-07 12:37:14 -0500

    Updating presentation slides.

--- documentation/Iteration2/hours_metrics.R
+++ documentation/Iteration2/hours_metrics.R
@@ -3,7 +3,7 @@
 library(ggplot2)
 library(reshape2)
 
-df <- data.frame(week=1:8, Manav=c(7,8,14,11,13,10,17,0), Vipul=c(6,10,11,13,14,10,10,0),Yuvaraj=c(5,13,17,15,27,6,6,0),Rachit=c(5,8,15,13,14,9,14,0),Alan=c(3,10,9,14,13,3,0,0),Christian=c(12,21,28,25,31,10,14,0),Tandhy=c(7,8,7,12,20,10,8,0))
+df <- data.frame(week=1:8, Manav=c(7,8,14,11,13,10,17,0), Vipul=c(6,10,11,13,14,10,10,0),Yuvaraj=c(5,13,17,15,27,6,6,0),Rachit=c(5,8,15,13,14,9,14,0),Alan=c(3,10,9,14,13,3,0,0),Christian=c(12,21,28,25,31,10,14,26),Tandhy=c(7,8,7,12,20,10,8,0))
 df <- melt(df, id.vars='week')
 
 g <- ggplot(df, aes(week, value, group=variable)) +
