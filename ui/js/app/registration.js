369b974fa0db3e25c064eca9684ef3cd183d9770
yuvarajbu <yuvarajg@bu.edu>
2013-12-04 23:21:21 -0500

    line 58 chked

--- ui/js/app/registration.js
+++ ui/js/app/registration.js
@@ -1,21 +1,21 @@
 function formValidation()
 {
-  
-var uid = $("input#userid").val();
-var passid = $("input#passid").val();
-var uname = $("input#firstname").val();
-var lastname = $("input#lastname").val();
-var uemail = $("input#email").val();
+	
+	var uid = $("input#userid").val();
+	var passid = $("input#passid").val();
+	var uname = $("input#firstname").val();
+	var lastname = $("input#lastname").val();
+	var uemail = $("input#email").val();
 
 if(allLetter(uname))
 {
-if(allLetter(lastname))
+	if(allLetter(lastname))
 {
-if(ValidateEmail(uemail))
+		if(ValidateEmail(uemail))
 {
-if(allLetterUid(uid))
+			if(allLetterUid(uid))
 {
-  regUser();
+				regUser();
 }
 }
 }
@@ -25,94 +25,94 @@ return false;
 
 function allLetter(uname)
 { 
-var letters = /^[A-Za-z]+$/;
-if(uname.value.match(letters))
-{
-return true;
-}
-else
-{
-document.getElementById('errorMsg').innerHTML = "Name cannot be empty and should have only alphabets!";
-uname.focus();
-return false;
-}
+	var letters = /^[A-Za-z]+$/;
+		if(uname.value.match(letters))
+			{
+				return true;
+			}
+		else
+			{
+				document.getElementById('errorMsg').innerHTML = "Name cannot be empty and should have only alphabets!";
+				uname.focus();
+				return false;
+			}
 }
 
 function ValidateEmail(uemail)
-{
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-if(uemail.value.match(mailformat))
-{
-return true;
-}
-else
-{
-document.getElementById('errorMsg').innerHTML = "You have entered an invalid email address!";
-uemail.focus();
-return false;
-}
+	{
+	var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(uemail.value.match(mailformat))
+			{
+				return true;
+			}
+		else
+			{
+				document.getElementById('errorMsg').innerHTML = "You have entered an invalid email address!";
+				uemail.focus();
+				return false;
+			}
 }
 
 function allLetterUid(uname)
-{ 
-var letters = /^[A-Za-z0-9_]{3,20}+$/;
-if(uname.value.match(letters))
-{
-return true;
-}
-else
-{
-document.getElementById('errorMsg').innerHTML = "Username can have only alphabets, numbers and _";
-uname.focus();
-return false;
-}
+	{ 
+	var letters = /^[A-Za-z0-9]+$/;
+		if(uname.value.match(letters))
+			{
+				return true;
+			}
+		else
+			{
+				document.getElementById('errorMsg').innerHTML = "Username can have only alphabets, numbers and _";
+				uname.focus();
+				return false;
+			}
 }
 
 function regUser(){
  
-              var  user = $("input#userid").val();
-              var pass = $("input#passid").val();
+							var  user = $("input#userid").val();
+							var pass = $("input#passid").val();
 
-                                $.ajax({
-                                        type: 'POST',
-                                        url: '/plm/rest/register',
-                                        contentType: 'application/json; charset=UTF-8',
-                                        accepts: {
-                                                text: 'application/json'
-                                        },
-                                        dataType: 'json',
-                                        data: JSON.stringify({
-                                                name: user,
-                                                password: pass
-                                        }),
-                                        success: function(data){
+																$.ajax({
+																				type: 'POST',
+																				url: '/plm/rest/register',
+																				contentType: 'application/json; charset=UTF-8',
+																				accepts: {
+																								text: 'application/json'
+																				},
+																				dataType: 'json',
+																				data: JSON.stringify({
+																								name: user,
+																								password: pass
+																				}),
+																				success: function(data){
 
-                                                /*the following code checks whether the entered userid is valid */
-                                                 if(data.code==0)
-                                                  {
-                                                    document.getElementById('errorMsg').innerHTML = "Registration Successful!! Welcome to PLM";
-                                                    window.location="login.html"; /*Redirect to the login page after succesful registration*/
-                                                  }
-                                                 else if(data.code==1)
-                                                 {
-                                                  document.getElementById('errorMsg').innerHTML = "Invalid Username or Password";/*displays error message*/
-                                     }
-                                       else { /* Usually internal error or other */
-                                           $("#errormsg").html(data.message);
-                                     }
-                                        },
-                                        error: function(data){
-                                                alert("error");
-                                        }
-                                });
-                }
+																								/*the following code checks whether the entered userid is valid */
+																								 if(data.code==0)
+																									{
+																										document.getElementById('errorMsg').innerHTML = "Registration Successful!! Welcome to PLM";
+																										window.location="login.html"; /*Redirect to the login page after succesful registration*/
+																									}
+																								 else if(data.code==1)
+																								 {
+																									document.getElementById('errorMsg').innerHTML = "Invalid Username or Password";/*displays error message*/
+																		 }
+																			 else { /* Usually internal error or other */
+																					 $("#errormsg").html(data.message);
+																		 }
+																				},
+																				error: function(data){
+																								alert("error");
+																				}
+																});
+								}
 
 function resetHandler() {
-        $("input#userid").val("");
-        $("input#passid").val("");
-        $("input#firstname").val("");
-        $("input#lastname").val("");
-        $("input#email").val("");
-        $("#errormsg").html("");
-        $("#errorMsg").html("");
+				$("input#userid").val("");
+				$("input#passid").val("");
+				$("input#firstname").val("");
+				$("input#lastname").val("");
+				$("input#email").val("");
+				$("#errormsg").html("");
+				$("#errorMsg").html("");
 }
